656a149c738f192737cfe39946479e2a215637dc
Fideline Ingabire <fidelining09@gmail.com>
2023-09-20 00:03:11 +0200

    concatinating string

--- Adding.js
+++ Adding.js
@@ -2,3 +2,5 @@ const AddingNumber = (num1,num2)=>{
     return num1+num2
 }
 console.log(AddingNumber(2,5)) 
+
+const addingnu
--- concatination.js
+++ concatination.js
@@ -1,2 +1,23 @@
 let myfirststring = "i am " + "fideline"
 console.log(myfirststring)
+
+let ourstring = "i come first "
+ourstring += "i come second"
+console.log(ourstring)
+
+let Mysrting = "first sentence " + " second sentence"
+console.log(Mysrting)
+
+let myname = "fideline"
+Mysrting = "my name is " +  myname  + " and i am fine"
+console.log(Mysrting)
+
+let anAdjective = " wondefull!"
+let mysrting = "learning code is "
+mysrting += anAdjective
+console.log(mysrting)
+
+
+
+
+
