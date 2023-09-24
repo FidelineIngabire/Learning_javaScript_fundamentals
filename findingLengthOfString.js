c71f8a439fcc5d68267bc78fad6bf49ea7fa9d7e
Fideline Ingabire <fidelining09@gmail.com>
2023-09-25 00:33:23 +0200

    findingLengthOfString

---
+++ findingLengthOfString.js
@@ -0,0 +1,4 @@
+let firstNameLength = 0
+let firstName = "fideline"
+firstNameLength = firstName. length
+console.log(firstNameLength)
---
+++ findingLetterOfString.js
@@ -0,0 +1,6 @@
+let firstLetterOfFirstName = ""
+let firstName = "fideline"
+ firstLetterOfFirstName = firstName[0]
+ console.log(firstLetterOfFirstName)
+
+
