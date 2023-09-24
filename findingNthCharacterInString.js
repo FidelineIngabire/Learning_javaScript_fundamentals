27cc41dddd87b957b371c38cca206d39e5e2e45c
Fideline Ingabire <fidelining09@gmail.com>
2023-09-25 00:57:49 +0200

    finding second letter of first name

---
+++ findingNthCharacterInString.js
@@ -0,0 +1,7 @@
+let firstName = "fideline"
+let secondLetterOffirstname = firstName[1]
+console.log(secondLetterOffirstname)
+
+let lastName = "ingabire"
+let thirdLetterOflastName = lastName[2]
+console.log(thirdLetterOflastName)
