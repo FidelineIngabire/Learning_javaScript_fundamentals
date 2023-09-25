1a5ec4536b5130aa4e0d7a25b41ee209bbb72773
Fideline Ingabire <fidelining09@gmail.com>
2023-09-26 00:18:45 +0200

    finding Nth to last letter in string

---
+++ findingNthToLastCharacterInString.js
@@ -0,0 +1,7 @@
+let firstName = "fideline"
+let thirdToLastLetterOffirstName = firstName[firstName.length-3]
+console.log(thirdToLastLetterOffirstName)
+
+let firstname = "fideline"
+let thirdToFirstLetterOffirstname = firstname[2]
+console.log(thirdToFirstLetterOffirstname)
