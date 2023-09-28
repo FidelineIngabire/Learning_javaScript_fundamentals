efd8befd2340815ae0f85ad1b5006ddd1df599c2
Fideline Ingabire <fidelining09@gmail.com>
2023-09-29 00:59:35 +0200

    arrangement data with sfift

---
+++ manipulateArraysWithShift.js
@@ -0,0 +1,7 @@
+let myArray = ["banana", "cake",["car"]]
+let myRemovedFrommyArray = myArray.shift(0)
+console.log(myArray)
+
+let myarray = [["amoris",3],["school",1]]
+let myremovedFrommyarray = myarray.shift(0)
+console.log(myarray)
